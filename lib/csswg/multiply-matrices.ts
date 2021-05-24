5654da7b16a280ee78a139d3398cd22ac266a2cc
Will Shown <v-wishow@microsoft.com>
2021-05-24 15:48:54 -0700

    Add Lch support libs.

---
+++ lib/csswg/multiply-matrices.ts
@@ -0,0 +1,43 @@
+/**
+ * Simple matrix (and vector) multiplication
+ * Warning: No error handling for incompatible dimensions!
+ * @author Lea Verou 2020 MIT License
+ */
+
+// [v-wishow]: Adjusted to export a TypeScript module. Retrieved on 24 May 2021
+// from https://drafts.csswg.org/css-color-4/multiply-matrices.js
+
+// A is m x n. B is n x p. product is m x p.
+export default function multiplyMatrices(A, B) {
+  let m = A.length;
+
+  if (!Array.isArray(A[0])) {
+    // A is vector, convert to [[a, b, c, ...]]
+    A = [A];
+  }
+
+  if (!Array.isArray(B[0])) {
+    // B is vector, convert to [[a], [b], [c], ...]]
+    B = B.map(x => [x]);
+  }
+
+  let p = B[0].length;
+  let B_cols = B[0].map((_, i) => B.map(x => x[i])); // transpose B
+  let product = A.map(row => B_cols.map(col => {
+    if (!Array.isArray(row)) {
+      return col.reduce((a, c) => a + c * row, 0);
+    }
+
+    return row.reduce((a, c, i) => a + c * (col[i] || 0), 0);
+  }));
+
+  if (m === 1) {
+    product = product[0]; // Avoid [[a, b, c, ...]]
+  }
+
+  if (p === 1) {
+    return product.map(x => x[0]); // Avoid [[a], [b], [c], ...]]
+  }
+
+  return product;
+}
---
+++ lib/lch/index.ts
@@ -0,0 +1,53 @@
+// [v-wishow]: Truncated to export only relevant functions and adjusted to export a TypeScript
+// module, some additional adjustments to remove alpha support. Retrieved on 24 May 2021
+// from https://raw.githubusercontent.com/LeaVerou/css.land/master/lch/lch.js
+
+import {LCH_to_sRGB} from "../csswg/utilities";
+
+export function alpha_to_string(a = 100) {
+  return (a < 100? ` / ${a}%` : "");
+}
+
+export function LCH_to_sRGB_string(l, c, h, forceInGamut = false) {
+  if (forceInGamut) {
+    [l, c, h] = force_into_gamut(l, c, h);
+  }
+
+  return "rgb(" + LCH_to_sRGB([+l, +c, +h]).map(x => {
+    return Math.round(x * 10000)/100 + "%";
+  }).join(" ") + ")";
+}
+
+export function force_into_gamut(l, c, h) {
+  // Moves an lch color into the sRGB gamut
+  // by holding the l and h steady,
+  // and adjusting the c via binary-search
+  // until the color is on the sRGB boundary.
+  if (isLCH_within_sRGB(l, c, h)) {
+    return [l, c, h];
+  }
+
+  let hiC = c;
+  let loC = 0;
+  const ε = .0001;
+  c /= 2;
+
+  // .0001 chosen fairly arbitrarily as "close enough"
+  while (hiC - loC > ε) {
+    if (isLCH_within_sRGB(l, c, h)) {
+      loC = c;
+    }
+    else {
+      hiC = c;
+    }
+    c = (hiC + loC)/2;
+  }
+
+  return [l, c, h];
+}
+
+export function isLCH_within_sRGB(l, c, h) {
+  var rgb = LCH_to_sRGB([+l, +c, +h]);
+  const ε = .000005;
+  return rgb.reduce((a, b) => a && b >= (0 - ε) && b <= (1 + ε), true);
+}
