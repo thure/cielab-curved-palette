9f5501e753d660709241390fa9fbffe077116c15
Will Shown <v-wishow@microsoft.com>
2021-06-03 14:32:21 -0700

    Move things to their right places.

--- src/controls/index.tsx
+++ src/controls/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import {
   Grid,
@@ -9,11 +9,64 @@ import {
 
 import { Controls } from './controls'
 import { Palette } from './palette'
+import { force_into_gamut } from '../lib/lch'
+import { LAB_to_sRGB } from '../lib/csswg/utilities'
 
 let onCurveUpdate
 
 export function curveUpdateHandler(points) {
   if (onCurveUpdate) onCurveUpdate(points)
+}
+
+function getTargetLightness(linearity, t) {
+  const cbrt = 6.3 * Math.cbrt(t * 1000 - 500) + 50
+  const line = t * 100
+  return Math.max(0, Math.min(100, cbrt + (line - cbrt) * linearity))
+}
+
+function getPaletteShades({ curvePoints, linearity, nShades }) {
+  if (curvePoints.length <= 2) return []
+
+  const paletteShades = []
+
+  let c = 0
+
+  for (let i = 0; i < nShades - 1; i++) {
+    const l = getTargetLightness(linearity, i / (nShades - 1))
+
+    while (l > curvePoints[c + 1][0]) {
+      c++
+    }
+
+    const [l1, a1, b1] = curvePoints[c]
+    const [l2, a2, b2] = curvePoints[c + 1]
+
+    const u = (l - l1) / (l2 - l1)
+
+    paletteShades[i] = [
+      l1 + (l2 - l1) * u,
+      a1 + (a2 - a1) * u,
+      b1 + (b2 - b1) * u,
+    ]
+  }
+
+  paletteShades[nShades - 1] = curvePoints[curvePoints.length - 1]
+
+  return paletteShades.map(([l, a, b]) => force_into_gamut(l, a, b))
+}
+
+function paletteShadesToHex(paletteShades) {
+  return paletteShades.map((lab) => {
+    return (
+      '#' +
+      LAB_to_sRGB(lab)
+        .map((x) => {
+          const channel = x < 0 ? 0 : Math.floor(x >= 1.0 ? 255 : x * 256)
+          return channel.toString(16).padStart(2, '0')
+        })
+        .join('')
+    )
+  })
 }
 
 const ControlsGrid = ({ sceneControls }) => {
@@ -28,6 +81,19 @@ const ControlsGrid = ({ sceneControls }) => {
     sceneControls.initialState.paletteNShades
   )
   const [paletteJSON, setPaletteJSON] = useState('')
+  const [paletteHexShades, setPaletteHexShades] = useState<string[]>([])
+
+  useEffect(() => {
+    const paletteShades = getPaletteShades({
+      curvePoints,
+      linearity: paletteDistributionLinearity,
+      nShades: paletteNShades,
+    })
+    const nextPaletteHexShades = paletteShadesToHex(paletteShades)
+    sceneControls.updateShades(paletteShades)
+    setPaletteJSON(JSON.stringify(nextPaletteHexShades, null, 2))
+    setPaletteHexShades(nextPaletteHexShades)
+  }, [curvePoints, paletteDistributionLinearity, paletteNShades])
 
   return (
     <Grid
@@ -53,11 +119,7 @@ const ControlsGrid = ({ sceneControls }) => {
       />
       <Palette
         {...{
-          sceneControls,
-          curvePoints,
-          paletteDistributionLinearity,
-          paletteNShades,
-          setPaletteJSON,
+          paletteHexShades,
         }}
       />
     </Grid>
--- src/controls/palette.tsx
+++ src/controls/palette.tsx
@@ -1,77 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Box, Flex } from '@fluentui/react-northstar'
-import { LAB_to_sRGB } from '../lib/csswg/utilities'
-import { force_into_gamut } from '../lib/lch'
 
-function getTargetLightness(linearity, t) {
-  const cbrt = 6.3 * Math.cbrt(t * 1000 - 500) + 50
-  const line = t * 100
-  return Math.max(0, Math.min(100, cbrt + (line - cbrt) * linearity))
-}
-
-function getPaletteShades({ curvePoints, linearity, nShades }) {
-  if (curvePoints.length <= 2) return []
-
-  const paletteShades = []
-
-  let c = 0
-
-  for (let i = 0; i < nShades - 1; i++) {
-    const l = getTargetLightness(linearity, i / (nShades - 1))
-
-    while (l > curvePoints[c + 1][0]) {
-      c++
-    }
-
-    const [l1, a1, b1] = curvePoints[c]
-    const [l2, a2, b2] = curvePoints[c + 1]
-
-    const u = (l - l1) / (l2 - l1)
-
-    paletteShades[i] = [
-      l1 + (l2 - l1) * u,
-      a1 + (a2 - a1) * u,
-      b1 + (b2 - b1) * u,
-    ]
-  }
-
-  paletteShades[nShades - 1] = curvePoints[curvePoints.length - 1]
-
-  return paletteShades.map(([l, a, b]) => force_into_gamut(l, a, b))
-}
-
-export const Palette = ({
-  sceneControls: { updateShades },
-  curvePoints,
-  paletteDistributionLinearity,
-  paletteNShades,
-  setPaletteJSON,
-}) => {
-  const paletteShades = getPaletteShades({
-    curvePoints,
-    nShades: paletteNShades,
-    linearity: paletteDistributionLinearity,
-  })
-
-  const paletteShadesHex = paletteShades.map((lab) => {
-    return (
-      '#' +
-      LAB_to_sRGB(lab)
-        .map((x) => {
-          const channel = x < 0 ? 0 : Math.floor(x >= 1.0 ? 255 : x * 256)
-          return channel.toString(16).padStart(2, '0')
-        })
-        .join('')
-    )
-  })
-
-  useEffect(() => {
-    if (paletteShades.length > 0) {
-      updateShades(paletteShades)
-      setPaletteJSON(JSON.stringify(paletteShadesHex, null, 2))
-    }
-  }, [curvePoints, paletteDistributionLinearity, paletteNShades])
-
+export const Palette = ({ paletteHexShades }) => {
   return (
     <Flex
       styles={{
@@ -80,7 +10,7 @@ export const Palette = ({
         overflow: 'hidden',
       }}
     >
-      {paletteShadesHex.map((hex, i) => {
+      {paletteHexShades.map((hex, i) => {
         return (
           <Box
             key={`shade_${i}`}
